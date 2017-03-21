a916f610c2b219410201c7af56a1be92e69b8f28
Simone Martorelli <simone.martorelli@icloud.com>
2017-03-21 11:52:37 +0100

    First

---
+++ PhysicsWithCoreMotion/PhysicsWithCoreMotion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhysicsWithCoreMotion
+//
+//  Created by Simone Martorelli on 21/03/17.
+//  Copyright © 2017 Simone Martorelli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
